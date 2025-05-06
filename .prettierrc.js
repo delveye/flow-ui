fac3d73b685aa8583ba8da8522cc082ff0aa47cb
Slobodan Zivanovic <207845948+slobodanzivanovic@users.noreply.github.com>
2025-05-06 23:16:02 +0200

    Initial

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSpacing: false,
+  printWidth: 80,
+  singleQuote: false,
+  semi: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+# Flow UI - Delveye Web UI Library
